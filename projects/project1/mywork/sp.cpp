00d57c5d18e93e7a8900a2ba297d611c3c36a4b7
Alaaddin Hiefield <alaaddin.hiefield@gmail.com>
2020-04-04 16:47:49 -0700

    worked on project one

--- projects/project1/mywork/sp.cpp
+++ projects/project1/mywork/sp.cpp
@@ -7,10 +7,14 @@
 // the user inputs for each entry.
 
 #include <iostream>
+#include <iomanip>  // for mm : ss output
+
 using namespace std;
 
 // Function Prototypes
-
+void OutputTimeInCorrectFormat(int minutes, int seconds);
+int GetAverageTimeInSeconds(int totalMinutes, int totalSeconds, int numberOfLaps);
+void FormatAverageTimeOutput(int averageTimeInSeconds);
 
 int main()
 {
@@ -23,35 +27,100 @@ int main()
     int numberOfLaps = 0;
     int averageLapTimeMinutes = 0;
     int averageLapTimeSeconds = 0;
-
+    int totalMinutes = 0;
+    int totalSeconds = 0;
+    
     do {
         cout << "Enter lap time (mm ss): ";
         cin >> minutes >> seconds;
         
-        if (minutes != 0 && seconds != 0) {
-            // Check fastest time in minutes and seconds
-            if (minutes > fastestTimeMinutes){
-                fastestTimeMinutes = minutes;
-                fastestTimeSeconds = seconds;
-            }
-            else if (minutes == fastestTimeMinutes && seconds > fastestTimeSeconds) {
-                fastestTimeSeconds = seconds;
-            }
-            else if (minutes < slowestTimeMinutes) {
-                slowestTimeMinutes = minutes;
-                slowestTimeSeconds = seconds;
-            }
-            else if (minutes == slowestTimeMinutes && seconds < slowestTimeSeconds) {
-                slowestTimeSeconds = seconds;
-            }
-            numberOfLaps++;
+        // Set fastest, slowest times accordingly on first lap
+        if (numberOfLaps == 0) {
+            fastestTimeMinutes = minutes;
+            fastestTimeSeconds = seconds;
+            slowestTimeMinutes = minutes;
+            slowestTimeSeconds = seconds;
         }
 
-        // Calculations
-        cout << "Total laps: " << numberOfLaps << endl;
-        if (averageLapTimeMinutes
+        // Check fastest time in minutes and seconds
+        if (minutes < fastestTimeMinutes){
+            fastestTimeMinutes = minutes;
+            fastestTimeSeconds = seconds;
+        }
+        // If seconds are faster, but minutes are the same
+        else if (minutes == fastestTimeMinutes && seconds < fastestTimeSeconds) {
+            fastestTimeSeconds = seconds;
+        }
+        else if (minutes > slowestTimeMinutes) {
+            slowestTimeMinutes = minutes;
+            slowestTimeSeconds = seconds;
+        }
+        else if (minutes == slowestTimeMinutes && seconds > slowestTimeSeconds) {
+            slowestTimeSeconds = seconds;
+        }
         
-    } while (userInputMinutes != 0 && userInputSeconds != 0)
+        // Calculations for average time
+        totalMinutes += minutes;
+        totalSeconds += seconds;
+        numberOfLaps++;
+        averageLapTimeMinutes = totalMinutes / numberOfLaps;
+        averageLapTimeSeconds = totalSeconds / numberOfLaps;
+
+        cout << "AVERAGE TIME: ";
+        int averageTimeInSeconds = GetAverageTimeInSeconds(totalMinutes, totalSeconds, numberOfLaps);
+        FormatAverageTimeOutput(averageTimeInSeconds);
+        cout << endl;
+        // Testing output
+        //cout << "Fastest Time: ";
+        //OutputTimeInCorrectFormat(fastestTimeMinutes, fastestTimeSeconds);
+        //cout << endl;
+        //OutputTimeInCorrectFormat(slowestTimeMinutes, slowestTimeSeconds);
+        //cout << endl;
+        //OutputTimeInCorrectFormat(averageLapTimeMinutes, averageLapTimeSeconds);
+        //cout << endl;
+        
+        // Calculations
+        //cout << "Total laps: " << numberOfLaps << endl;
+        //cout << "END LAP" << endl;
+        
+    } while (minutes > 0 || seconds > 0);
+
+    numberOfLaps -= 1;
+    cout << numberOfLaps << endl;
+    cout << "END RACE" << endl;
 
     return 0;
+}
+
+// Function Definitions
+
+// OutputTimeInCorrectFormat
+// Passed in values are output to console in correct lap time format.
+void OutputTimeInCorrectFormat(int minutes, int seconds)
+{
+    cout << setfill('0') << setw(2) << minutes << ":" << setw(2) <<  seconds;
+}
+
+// GetAverageMinutes
+int GetAverageTimeInSeconds(int totalMinutes, int totalSeconds, int numberOfLaps)
+{
+    int totalTimeInSeconds = totalSeconds + (totalMinutes * 60);
+    int averageTotalTimeInSeconds = totalSeconds / numberOfLaps;
+
+    cout << "TOTAL MINUTES: " << totalMinutes << endl;
+    cout << "TOTAL SECONDS: " << totalSeconds << endl;
+    cout << "TOTAL LAPS: " << numberOfLaps << endl;
+    cout << "TOTAL TIME IN SECONDS: " << totalSeconds + totalMinutes * 60 << endl;
+    cout << "AVG TOTAL IN SECONDS: " << (totalSeconds + totalMinutes * 60) / numberOfLaps << endl; 
+    return (totalSeconds + totalMinutes * 60) / numberOfLaps;
+}
+
+void FormatAverageTimeOutput(int averageTimeInSeconds)
+{
+    int averageMinutes = 0;
+    int averageSeconds = 0;
+    // Math here
+    averageMinutes = averageTimeInSeconds / 60;
+    averageSeconds = averageTimeInSeconds % 60;
+    OutputTimeInCorrectFormat(averageMinutes, averageSeconds);
 }
