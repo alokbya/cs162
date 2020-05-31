2598ee19be420c8928cb0c0822855fbb2f5b44de
Alaaddin Hiefield <alaaddin.hiefield@gmail.com>
2020-05-30 22:37:29 -0700

    completed lab06, starting project5

--- labs/lab06/list.cpp
+++ labs/lab06/list.cpp
@@ -8,66 +8,102 @@ LinkedList::LinkedList()
 }   
 LinkedList::~LinkedList()
 {
-    // Node *current;
-    // current = head;
-    // while (current != nullptr)
-    // {
-    //     current = current->next;
-    //     delete current->prev;
-    // }
-    // delete head;
-    // delete tail;
+    Node *current = head;
+    Node *next = nullptr;
+    while(current)
+    {
+        next = current->next;
+        delete current;
+        current = next;
+    }
+    // delete current;
 }
+
 bool LinkedList::addAtBeginning(int val)
 {
-    cout << "adding " << val << endl;
-    Node *newNode;                      // declare new node pointer
-    newNode = new Node;                 // allocate memory for new node
-    if(head == nullptr)                 // if the list has 0 nodes 
-    {
-        newNode->data = val;            // assign newNode->data = val
-        newNode->prev = nullptr;        //  
-        newNode->next = nullptr;
-        
-        head = newNode;
-        tail = newNode;
-        cout << "tail is " << tail->data << endl;
-    }
-    else                                // if the list has > 0 nodes
-    {
-        newNode->data = val;
-        newNode->next = head;
-        head->prev = newNode;
-        newNode->prev = nullptr;
-        head = newNode;
-        cout << "tail is " << tail->data << endl;
-    }
+    Node *n = nullptr;
+    n = new Node;
+    n->data = val;
+    n->prev = nullptr;                      
+    n->next = nullptr;                  // causing major headache, MUST initialize
     
-    // printForward();
-
+    if(head)                            // if head is not a nullptr;
+    {
+        n->next = head;
+        head->prev = n;
+    }
+    else
+        tail = n;
+    head = n;
+    return true;
 }
+
 bool LinkedList::remove(int val)
 {
     // iterate through the list until last node (nullptr)
+    // if val is found, delete node, return true
+    bool found = false;
+    Node *current = head;
+    Node *trailCurrent = nullptr;
+
+    Node *toDelete = nullptr;
+    while(current && !found)
+    {
+        if(current->data == val)
+        { 
+            toDelete = current;
+            found = true;
+        }
+        current = current->next;
+    }
+    // if the list is empty toDelete == head == nullptr
+    if(!toDelete)
+        found = false;
+    // if item is in first node of the list
+    else if(toDelete == head)
+    {   
+        current = head;
+        head = head->next;
+        head->prev = nullptr;
+        delete current;
+    }
+    else if(toDelete == tail)
+    {
+        current = tail;
+        trailCurrent = current->prev;
+        trailCurrent->next = nullptr;
+        tail = trailCurrent;
+        delete current;
+    }
+    else                                                                        // toDelete is in the middle
+    {
+        current = toDelete;
+        trailCurrent = current->prev;
+        trailCurrent->next = current->next;
+        current->next->prev = trailCurrent;
+        delete current;
+    }
+    
+    return found;
 }
+
 void LinkedList::printForward() const
 {
-    Node *current;
-    current = new Node;
-    current = head;
-    while (current != nullptr)
+    Node *current = nullptr;
+    if(head)
+        current = head;
+    while(current)
     {
         cout << current->data << " ";
         current = current->next;
     }
     cout << endl;
 }
+
 void LinkedList::printBackward() const
 {
-    Node *current;
-    current = new Node ;
-    current = tail;
-    while (current != nullptr)
+    Node *current = tail;
+    while (current)
     {
         cout << current->data << " ";
         current = current->prev;
